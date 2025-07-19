9714c767ce276c543432d6c37677422b969c1800
Leader-Islombek <iviuzbek@gmail.com>
2025-07-19 04:12:26 -0700

    Fix ADMIN_ID NoneType error

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID")) # type: ignore
+ADMIN_ID = os.getenv("ADMIN_ID")
+
+if ADMIN_ID is None:
+    raise ValueError("ADMIN_ID environment variable not set.")
+else:
+    ADMIN_ID = int(ADMIN_ID)
